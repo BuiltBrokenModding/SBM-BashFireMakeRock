e90fcd83134467dad6ec43558a08b530f52c0b04
Thiakil <xander@thiakil.com>
2019-03-27 20:29:05 +0800

    remove the redundant RayTraceResult creation and thus the null pointer. Fixes #2

--- src/main/java/com/builtbroken/bashrockmakefire/logic/EventHandler.java
+++ src/main/java/com/builtbroken/bashrockmakefire/logic/EventHandler.java
@@ -22,63 +22,55 @@ public class EventHandler
 {
 
     public static HashMap<BlockPos, ClickData> clickDataHashMap = new HashMap<>();
-    public static List<Block> supportedBlocks = new ArrayList();
-    public static List<IBlockState> supportedBlockStates = new ArrayList();
+    public static List<Block> supportedBlocks = new ArrayList<>();
+    public static List<IBlockState> supportedBlockStates = new ArrayList<>();
 
     @SubscribeEvent
     public static void onBlockPunch(PlayerInteractEvent.LeftClickBlock event)
     {
         try
         {
-            if (!event.getWorld().isRemote)
+            //Get the position and block
+            final BlockPos clickPos = event.getPos();
+            final IBlockState blockTarget = event.getWorld().getBlockState(clickPos);
+            if (isBlockSupported(blockTarget))
             {
-                //Get what we are clicking TODO check if we need a ray trace as event stores face and block
-                final RayTraceResult result = new RayTraceResult(event.getHitVec(), event.getFace(), event.getPos());
-                if (result.typeOfHit == RayTraceResult.Type.BLOCK)
+                if (clickDataHashMap.containsKey(clickPos))
                 {
-                    //Get the position and block
-                    final BlockPos clickPos = result.getBlockPos();
-                    final IBlockState blockTarget = event.getWorld().getBlockState(clickPos);
-                    if (isBlockSupported(blockTarget))
+                    if (clickDataHashMap.get(clickPos).click())
                     {
-                        if (clickDataHashMap.containsKey(clickPos))
+                        //Check if we can place fire
+                        final BlockPos firePos = clickPos.up();
+                        final IBlockState blockAbove = event.getWorld().getBlockState(firePos);
+                        if (blockAbove.getBlock().isReplaceable(event.getWorld(), firePos))
                         {
-                            if (clickDataHashMap.get(clickPos).click())
+                            event.setCanceled(true);
+                            event.getWorld().setBlockState(firePos, Blocks.FIRE.getDefaultState());
+                            if (event.getEntityPlayer().getGameProfile() != null)
                             {
-                                //Check if we can place fire
-                                final BlockPos firePos = clickPos.up();
-                                final IBlockState blockAbove = event.getWorld().getBlockState(firePos);
-                                if (blockAbove.getBlock().isReplaceable(event.getWorld(), firePos))
-                                {
-                                    event.setCanceled(true);
-                                    event.getWorld().setBlockState(firePos, Blocks.FIRE.getDefaultState());
-                                    if (event.getEntityPlayer().getGameProfile() != null)
-                                    {
-                                        BashFireMakeRock.LOGGER.info(
-                                                event.getEntityPlayer().getGameProfile().getName() + " [" + event.getEntityPlayer().getGameProfile().getId() + "]"
-                                                        + " created fire at " + firePos
-                                                        + " in world " + event.getWorld().getWorldInfo().getWorldName()
-                                                        + " dim: " + event.getWorld().provider.getDimension());
-                                    }
-                                }
-
-                                //Remove as we are done
-                                clickDataHashMap.remove(clickPos);
+                                BashFireMakeRock.LOGGER.info(
+                                        event.getEntityPlayer().getGameProfile().getName() + " [" + event.getEntityPlayer().getGameProfile().getId() + "]"
+                                                + " created fire at " + firePos
+                                                + " in world " + event.getWorld().getWorldInfo().getWorldName()
+                                                + " dim: " + event.getWorld().provider.getDimension());
                             }
                         }
-                        else
-                        {
-                            clickDataHashMap.put(clickPos, new ClickData(clickPos));
-                        }
+
+                        //Remove as we are done
+                        clickDataHashMap.remove(clickPos);
                     }
                 }
-
-                //Clear dead entries TODO check for performance hit on larger servers
-                clickDataHashMap.keySet().stream()
-                        .filter(e -> clickDataHashMap.get(e).isDead())
-                        .collect(Collectors.toList())
-                        .forEach(e -> clickDataHashMap.remove(e));
+                else
+                {
+                    clickDataHashMap.put(clickPos, new ClickData(clickPos));
+                }
             }
+
+            //Clear dead entries TODO check for performance hit on larger servers
+            clickDataHashMap.keySet().stream()
+                    .filter(e -> clickDataHashMap.get(e).isDead())
+                    .collect(Collectors.toList())
+                    .forEach(e -> clickDataHashMap.remove(e));
         }
         catch (Exception e)
         {
